8eb218fda35980f23e8dc45f67fb32e45bb070d9
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-10-14 09:42:21 +0530

    initial commit

---
+++ src/screens/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Shortlink</Text>
+    </View>
+  );
+};
+
+export default Home;
